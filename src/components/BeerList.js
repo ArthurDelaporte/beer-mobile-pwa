ae3fdf1383c67a2c765fde5251462c5114af5249
DELAPORTE <arthur.delaporte@eemi.com>
2024-11-22 12:04:04 +0100

    Grosse màj de l'affichage des bières et du csv

--- src/components/BeerList.js
+++ src/components/BeerList.js
@@ -18,16 +18,27 @@ const BeerList = () => {
     }, []);
 
     return (
-        <div>
-            <h1>Liste des Bières</h1>
-            <ul>
+        <div className="beer-list">
+            <h1 className="title">Liste des Bières</h1>
+            <ul className="grid">
                 {beers.map((beer, index) => (
-                    <li key={index}>
-                        <div>
-                            <strong>{beer.name}</strong>
-                            <span>{beer.style}</span>
+                    <li key={index} className="beer-card">
+                        {/* Image */}
+                        <img
+                            src={'/img/beers/' + beer.image_url}
+                            alt={beer.name}
+                            className="beer-image"
+                        />
+
+                        {/* Informations */}
+                        <div className="beer-info">
+                            <h2 className="beer-name">{beer.name}</h2>
+                            <p className="beer-tagline">{beer.tagline}</p>
+                            <p className="beer-style">{beer.style}</p>
+                            <p className="beer-attributes">
+                                {beer.size} • {beer.price}€ • {beer.abv}%
+                            </p>
                         </div>
-                        <div>{beer.abv}% ABV</div>
                     </li>
                 ))}
             </ul>
